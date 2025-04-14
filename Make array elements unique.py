b6c6e850392bc15b9d2dc637afd61e452ce006d9
Alok Singh <133560050+githubAloksingh@users.noreply.github.com>
2025-04-14 18:27:56 +0530

    Make array elements unique.py

--- Make array elements unique.py
+++ Make array elements unique.py
@@ -5,7 +5,7 @@ class Solution:
         # Code here
         arr.sort()
         moves=0
-        for i in range(1,len(arr)):
+        for i in range(1,len(arr)): 
             if arr[i]<=arr[i-1]:
                 increment=arr[i-1]-arr[i]+1
                 arr[i]+=increment
