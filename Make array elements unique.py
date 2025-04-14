5a7d92663fd1d4a79c0aff12dcdca11b25bb0f50
Alok Singh <133560050+githubAloksingh@users.noreply.github.com>
2025-04-14 18:26:52 +0530

    B-Create Make array elements unique.py

---
+++ Make array elements unique.py
@@ -0,0 +1,29 @@
+#User function Template for python3
+
+class Solution:
+    def minIncrements(self, arr): 
+        # Code here
+        arr.sort()
+        moves=0
+        for i in range(1,len(arr)):
+            if arr[i]<=arr[i-1]:
+                increment=arr[i-1]-arr[i]+1
+                arr[i]+=increment
+                moves+=increment
+        return moves
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+
+    T = int(input())
+    while T > 0:
+        arr = [int(i) for i in input().split()]
+        ob = Solution()
+        print(ob.minIncrements(arr))
+        print("~")
+        T -= 1
+
+# } Driver Code Ends
